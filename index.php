8560a31002fa60c609065496892cb6740d8ba505
Bruno Campos Santos <contato@hxtutors.com.br>
2015-08-01 00:21:11 -0300

    Add Container-DI

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	use DI\ContainerBuilder;
+
 	ob_start();
 
 	ini_set('display_errors', 1); 
@@ -18,6 +20,12 @@
 	}
 
 	require_once($composer_autoload);
+
+	/**
+	 * Container de injeção de dependência
+	 */
+	$builder = new ContainerBuilder();
+	$container = $builder->build();
 
 	//Start da sessão
 	StartSession::sec_session_start();
