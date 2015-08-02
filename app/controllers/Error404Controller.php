b4b171e7a657dc778c893e67a3d3ffa880da8f3e
Bruno Campos Santos <contato@hxtutors.com.br>
2015-08-02 17:07:22 -0300

    Fix Default files

--- app/controllers/Error404Controller.php
+++ app/controllers/Error404Controller.php
@@ -2,11 +2,6 @@
 
 class Error404Controller extends \HXPHP\System\Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load('Auth');
-	}
 	public function indexAction()
 	{
 		$this->view->setTitle('Oops! Nada encontrado!');
