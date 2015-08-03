acbe5f97b171c56a62b6ad6d75ab6c47dccee594
Bruno Campos Santos <contato@hxtutors.com.br>
2015-08-03 17:41:25 -0300

    Isolação das configurações

--- app/config.php
+++ app/config.php
@@ -1,2 +1,14 @@
 <?php
+	
+	$configs = new HXPHP\System\Configs\Config;
 
+	$configs->env->add('production');
+	$configs->env->production->database->setConnectionData(array(
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => '',
+		'dbname' => 'teste'
+	));
+	$configs->define->setDefaultEnv('production');
+
+	return $configs;
--- index.php
+++ index.php
@@ -21,21 +21,7 @@
 	//Start da sessão
 	HXPHP\System\Services\StartSession::sec_session_start();
 
-	//Configurações
-	$configs = new HXPHP\System\Configs\Config;
-
-	$configs->env->add('production');
-	$configs->env->production->database->setConnectionData(array(
-		'host' => 'localhost',
-		'user' => 'root',
-		'password' => '',
-		'dbname' => 'teste'
-	));
-
-	$configs->define->setDefaultEnv('production');
-
-	var_dump($configs->database);
-
-	$app = new HXPHP\System\App($configs);
+	//Inicio da aplicação
+	$app = new HXPHP\System\App(require_once APP_PATH . 'config.php');
 	$app->ActiveRecord();
 	$app->run();
