d4c52585e759c4082d90591e25af3d35acd7bc2b
Bruno Campos Santos <contato@hxtutors.com.br>
2015-08-03 13:23:13 -0300

    Add Configurações globais padrão e remoção das configurações de arquivos estáticos

--- src/HXPHP/System/Configs/Global.php
+++ src/HXPHP/System/Configs/Global.php
@@ -7,7 +7,6 @@ class Global
 	public $models;
 	public $views;
 	public $controllers;
-	public $static = array();
 
 	public function __construct()
 	{
@@ -15,5 +14,15 @@ class Global
 		$this->views = new stdClass;
 		$this->controllers = new stdClass;
 
+		//Models
+		$this->models->directory = APP_PATH . DS . 'models' . DS;
+
+		//Views
+		$this->views->directory = APP_PATH . DS . 'views' . DS;
+		$this->views->extension = '.phtml';
+
+		//Controller
+		$this->controllers->directory = APP_PATH . DS . 'controllers' . DS;
+		$this->controllers->notFound = 'Error404';
 	}
 }
