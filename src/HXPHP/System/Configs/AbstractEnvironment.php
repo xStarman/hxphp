0a56d02e6b342c657263e18d7985b34d59499cb8
Bruno Campos Santos <contato@hxtutors.com.br>
2015-08-04 14:11:12 -0300

    Remoção da injeção de dependência desnecessária

--- src/HXPHP/System/Configs/AbstractEnvironment.php
+++ src/HXPHP/System/Configs/AbstractEnvironment.php
@@ -7,15 +7,14 @@ use HXPHP\System\Configs\Modules as Modules;
 abstract class AbstractEnvironment
 {
 	public $baseURI;
-	private $load;
 
 	public function __construct()
 	{
 
 		$this->baseURI = '/hxphp/';
 
-		$this->load = new LoadModules;
+		$load = new LoadModules;
 
-		return $this->load->loadModules($this);
+		return $load->loadModules($this);
 	}
 }
