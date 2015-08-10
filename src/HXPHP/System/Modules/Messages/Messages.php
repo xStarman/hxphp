ee32a9e072badd893ae4c1b89ff45b2f34bac45b
Bruno Campos Santos <contato@hxtutors.com.br>
2015-08-10 20:30:57 -0300

    Remoção de métodos obsoletos e correção nos comentários do módulo de mensagens

--- src/HXPHP/System/Modules/Messages/Messages.php
+++ src/HXPHP/System/Modules/Messages/Messages.php
@@ -40,9 +40,9 @@ class Messages
 	}
 
 	/**
-	 * Retorna o ARRAY com o grupo de mensagens de acordo com o bloco
+	 * Retorna uma instância do objeto Template configurada mediante o bloco informado
 	 * @param  string $param Nome do bloco
-	 * @return array         Conteúdo do bloco
+	 * @return object        Instância do objeto Template
 	 */
 	public function __get($param)
 	{
@@ -53,43 +53,5 @@ class Messages
 		}
 
 		throw new \Exception("O bloco <'$param'> nao foi encontrado no template <'$this->file'>.", 1);
-	}
-
-	/**
-	 * Retorna os avisos
-	 * @param  string $code   Código do aviso
-	 * @param  array  $params Parâmetros de substituição
-	 * @return array          Estrutura do aviso selecionado
-	 */
-	public function getAlert($code, array $params = array())
-	{
-		if (isset($this->alerts[$code])){
-			$alert = $this->alerts[$code];
-
-			if ( ! empty($params) )
-				$alert['message'] = vsprintf($alert['message'], $params);
-
-			return array_values($alert); //RETORNO DIFERENTE
-		}
-		return null;
-	}
-
-	/**
-	 * Retorna as mensagens
-	 * @param  string $code   Código da mensagem
-	 * @param  array  $params Parâmetros de substituição
-	 * @return mixed          Estrutura da mensagem selecionada
-	 */
-	public function getMessage($code, array $params = array())
-	{
-		if (isset($this->messages[$code])){
-			$message = $this->messages[$code];
-
-			if ( ! empty($params) )
-				$message['message'] = vsprintf($message['message'], $params);
-
-			return $message; //RETORNO DIFERENTE
-		}
-		return null;
 	}
 }
