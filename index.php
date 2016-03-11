05008c7be4acb97d6a178c170eca842cb0f1f577
Gabriel Caruso <carusogabriel34@gmail.com>
2016-03-11 16:49:36 -0300

    Adição de configurações de local e tempo de execução

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 	ob_start();
 
-	ini_set('display_errors', 1); 
+	ini_set('display_errors', 1);
+        set_time_limit(0);
+
 	date_default_timezone_set('America/Sao_Paulo');
+        setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT_PATH', dirname(__FILE__) . DS);
