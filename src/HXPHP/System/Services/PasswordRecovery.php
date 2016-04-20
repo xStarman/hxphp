a6dd47020fe62b2d627565cf7cfb9b202d61dc19
Bruno Campos Santos <contato@hxtutors.com.br>
2016-04-20 19:02:18 -0300

    Update PasswordRecovery Service

--- src/HXPHP/System/Services/PasswordRecovery.php
+++ src/HXPHP/System/Services/PasswordRecovery.php
@@ -2,28 +2,13 @@
 
 namespace HXPHP\System\Services;
 
-use HXPHP\System\Modules\Messages\Messages;
-
 class PasswordRecovery
 {
-
-	/**
-	 * Injeção do módulo de Mensagens
-	 * @var object
-	 */
-	public $messages;
-
-	/**
-	 * Resultados do processo
-	 * @var boolean
-	 */
-	public $status = false;
-
 	/**
 	 * Link de redefinição
 	 * @var null
 	 */
-	private $link = null;
+	public $link = null;
 
 	/**
 	 * Código alfanumérico de autenticação da requisição
@@ -32,14 +17,12 @@ class PasswordRecovery
 	public $token;
 
 	/**
-	 * Método construtor
+	 * Define o link
+	 * @param string $link Prefixo do link c/ barra no final Ex.: http://www.site.com.br/esqueci-a-senha/redefinir/
 	 */
-	public function __construct()
+	public function __construct($link)
 	{
-		$this->messages = new Messages('password-recovery');
-		$this->messages->setBlock('alerts');
-		
-		return $this;
+		$this->setLink($link);
 	}
 
 	/**
@@ -52,36 +35,11 @@ class PasswordRecovery
 
 	/**
 	 * Define o link
-	 * @param string $link Prefixo do link
+	 * @param string $link Prefixo do link c/ barra no final
 	 */
 	public function setLink($link)
 	{
 		$this->generateToken();
-		$this->link = $link.$this->token;
-	}
-
-	/**
-	 * Envia a mensagem de redefinição de senha
-	 * @param  string $name  Nome do usuário
-	 * @param  string $email E-mail do usuário
-	 * @return array         Mensagem resultante do processo
-	 */
-	public function sendRecoveryLink($name, $email)
-	{
-		if (is_null($this->link))
-			return $this->messages->getAlert('link-nao-definido');
-
-		$message = $this->messages->getMessage('link-enviado', array(
-			$name,
-			$this->link,
-			$this->link
-		));
-
-		if ($this->messages->sendEmail($email, $message)) {
-			$this->status = true;
-			return $this->messages->getAlert('link-enviado');
-		}
-
-		return $this->getMessage('email-nao-enviado');
+		$this->link = $link . $this->token;
 	}
 }
