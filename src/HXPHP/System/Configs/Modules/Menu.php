3fd98ab18871e8dee53c90b3ccfd3809cf8ce40e
Bruno Campos Santos <contato@hxtutors.com.br>
2016-04-25 00:15:24 -0300

    Add Menu Module Config

---
+++ src/HXPHP/System/Configs/Modules/Menu.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HXPHP\System\Configs\Modules;
+
+class Menu
+{
+	private $configs = array(
+		'container' => false,
+		'container_id' => '',
+		'container_class' => '',
+		'menu_id' => 'menu',
+		'menu_class' => 'menu',
+		'menu_active_class' => 'active',
+		'menu_dropdown_class' => 'dropdown',
+		'link_dropdown_class' => 'dropdown-toggle',
+		'link_attrs' => array(
+			'data-toggle' => 'dropdown'
+		),
+		'dropdown_class' => 'dropdown-menu',
+		'dropdown_active_class' => 'active'
+	);
+}
