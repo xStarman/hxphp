6fa65cd9770c4af65aa57a43e8c0b435c96989cd
Bruno Campos Santos <contato@hxtutors.com.br>
2016-05-02 23:38:36 -0300

    Alteração no método load e teste bem sucedido

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,4 +2,13 @@
 
 class IndexController extends \HXPHP\System\Controller
 {
+	public function indexAction()
+	{
+		$this->load('Modules\Messages', 'auth');
+		$this->messages->setBlock('alerts');
+
+		$alert = $this->messages->getByCode('conta-em-uso');
+
+		$this->load('Helpers\Alert', $alert);
+	}
 }
