c7f3185e525e4c4f36a9ee6d100642035c009cd4
Bruno Campos Santos <contato@hxtutors.com.br>
2016-05-02 23:39:28 -0300

    Remove tests

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,13 +2,4 @@
 
 class IndexController extends \HXPHP\System\Controller
 {
-	public function indexAction()
-	{
-		$this->load('Modules\Messages', 'auth');
-		$this->messages->setBlock('alerts');
-
-		$alert = $this->messages->getByCode('conta-em-uso');
-
-		$this->load('Helpers\Alert', $alert);
-	}
 }
