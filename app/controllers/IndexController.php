4d383d61d9143762a67b521cc2496afbb41987fe
Bruno Santos <contato@hxtutors.com.br>
2016-05-05 14:59:33 -0300

    Add testes

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,5 +5,14 @@ class IndexController extends \HXPHP\System\Controller
 	public function indexAction()
 	{
 		$this->load('Helpers\Menu', $this->request, $this->configs);
+
+		$this->load('Helpers\Alert', array(
+			'danger',
+			'Título',
+			array(
+				'Mensagem 1',
+				'Mensagem 2'
+			)
+		));
 	}
 }
