fe5f6d98937d4a5830c10389a3c0fa7b5314d285
Bruno Santos <contato@hxtutors.com.br>
2016-05-05 14:08:27 -0300

    Conclusão da segmentação de responsabilidades do MenuHelper

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,6 +4,6 @@ class IndexController extends \HXPHP\System\Controller
 {
 	public function indexAction()
 	{
-		$this->load('Helpers\Menu', $this->request, $this->configs, 'administrator');
+		$this->load('Helpers\Menu', $this->request, $this->configs);
 	}
 }
