1a05bf176817a0277c5d8b3761d527453c8ca6ee
Bruno Santos <contato@hxtutors.com.br>
2016-05-08 20:16:14 -0300

    Add Teste

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,6 +4,16 @@ class IndexController extends \HXPHP\System\Controller
 {
 	public function indexAction()
 	{
+		$this->load(
+			'Services\Auth',
+			$this->configs->auth->after_login,
+			$this->configs->auth->after_logout,
+			false
+		);
+
+		$this->auth->login(1, 'teste');
+
+		var_dump($this->auth->login_check());
 		$this->load('Helpers\Menu', $this->request, $this->configs);
 	}
 }
