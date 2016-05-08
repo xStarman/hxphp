addcbe1c96c1e3d22801c71bad879bb08061343b
Bruno Santos <contato@hxtutors.com.br>
2016-05-08 20:20:04 -0300

    Add parâmetro subfolder no carregamento do serviço

--- app/controllers/admin/IndexController.php
+++ app/controllers/admin/IndexController.php
@@ -4,6 +4,17 @@ class IndexController extends \HXPHP\System\Controller
 {
 	public function indexAction()
 	{
+		$this->load(
+			'Services\Auth',
+			$this->configs->auth->after_login,
+			$this->configs->auth->after_logout,
+			true,
+			$this->request->subfolder
+		);
+
+		$this->auth->login(2, 'teste');
+		var_dump($this->auth->login_check());
+
 		$this->view->setPath('admin/havefun');
 	}
 }
