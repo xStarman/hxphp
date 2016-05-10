57e217c20452c7444e7238de870aea8dce79ca6e
Bruno Santos <contato@hxtutors.com.br>
2016-05-10 14:29:42 -0300

    Add Exemplo do novo funcionamento

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,5 +5,14 @@ class IndexController extends \HXPHP\System\Controller
 	public function indexAction()
 	{
 		$this->load('Helpers\Menu', $this->request, $this->configs);
+
+		$this->load('Services\Email');
+		$this->email->setFrom($this->configs->mail->getFrom());
+
+		print_r($this->email->send(
+			'bruno@hxphp.com.br',
+			'Hello World!',
+			'Mensagem <strong>com HTML</strong>'
+		));
 	}
 }
