f34137960019960195636572796be307b6b129cc
Bruno Santos <contato@hxtutors.com.br>
2016-05-17 22:24:25 -0300

    Fix version

--- index.php
+++ index.php
@@ -19,9 +19,8 @@
 	if ( ! file_exists($composer_autoload))
 		die('Execute o comando: composer install');
 
-	if (version_compare(PHP_VERSION, '6.4.0', '<'))
+	if (version_compare(PHP_VERSION, '5.4.0', '<'))
 		die('Atualize seu PHP para a vers&atilde;o: 5.4.0 ou superior.');
-
 
 	require_once($composer_autoload);
 
