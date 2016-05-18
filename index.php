4ef24ced8a45565502cd82bba7af8545cc29256e
Bruno Santos <contato@hxtutors.com.br>
2016-05-17 22:23:02 -0300

    Add version_compare

--- index.php
+++ index.php
@@ -16,9 +16,12 @@
 	 */
 	$composer_autoload = 'vendor' . DS . 'autoload.php';
 
-	if ( ! file_exists($composer_autoload)) {
+	if ( ! file_exists($composer_autoload))
 		die('Execute o comando: composer install');
-	}
+
+	if (version_compare(PHP_VERSION, '6.4.0', '<'))
+		die('Atualize seu PHP para a vers&atilde;o: 5.4.0 ou superior.');
+
 
 	require_once($composer_autoload);
 
