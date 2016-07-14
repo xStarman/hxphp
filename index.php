60b37dacbf87348082805f6d61d47bbbc40f47ef
Gabriel Caruso <carusogabriel34@gmail.com>
2016-07-14 16:06:06 -0300

    Correção display de errors gerados pelo php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 	ob_start();
 
-	set_time_limit(0);
+	ini_set('display_errors', 1);
+        set_time_limit(0);
 
 	date_default_timezone_set('America/Sao_Paulo');
 	setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
