cfd4143a6ef076a44d0c6c8d3eaf3f7f2eb3f583
METADIL MÓVEIS - Caio Lesnock <caio@metadil.local>
2016-11-21 14:20:45 -0200

    Acréscimo de tempo de expiração para as sessões

--- src/HXPHP/System/Storage/Session/Session.php
+++ src/HXPHP/System/Storage/Session/Session.php
@@ -13,11 +13,17 @@ class Session implements \HXPHP\System\Storage\StorageInterface
 	 * Método que cria uma sessão
 	 * @param string $name  Nome da sessão
 	 * @param string $value Conteúdo da sessão
+     * @param int $timeout  Tempo de expiração da sessão
 	 */
-	public function set($name, $value)
+	public function set($name, $value, $timeout = null)
 	{
 		$_SESSION[self::PREFIX][$name] = $value;
-
+        
+        if ($timeout && is_int($timeout)) {
+            $_SESSION[self::PREFIX][$name.'_created_at'] = time();
+            $_SESSION[self::PREFIX][$name.'_timeout'] = $timeout;
+        }
+        
 		return $this;
 	}
 
@@ -28,8 +34,12 @@ class Session implements \HXPHP\System\Storage\StorageInterface
 	 */
 	public function get($name)
 	{
-		if ($this->exists($name))
-			return $_SESSION[self::PREFIX][$name];
+		if ($this->exists($name)) {            
+            if (!$this->hasExpired($name))
+                return $_SESSION[self::PREFIX][$name];
+            else
+                return false;
+        }
 
 		return null;
 	}
@@ -43,6 +53,36 @@ class Session implements \HXPHP\System\Storage\StorageInterface
 	{
 		return isset($_SESSION[self::PREFIX][$name]);
 	}
+    
+    /**
+    * Verifica se uma sessão já expirou
+    * @param string $name   Nome da sessão
+    * @return boolean       Sessão expirada ou não
+    */
+    public function hasExpired($name)
+    {
+        if (!$this->exists($name.'_timeout'))
+            return false;
+        
+        if (!$this->exists($name.'_created_at'))
+            throw new \LogicException('A sessão < '.$name.'_created_at > não existe.');
+        
+        if ($this->get($name.'_created_at') + $this->get($name.'_timeout') < time())
+            return true;
+    }
+    
+    /**
+    * Quantidade de tempo restante para o timeout de uma sessão
+    * @param string $name   Nome da sessão
+    * @return date          Quantidade de tempo restante para o timeout
+    */
+    public function getTimeLeftOf($name)
+    {
+        if ($this->hasExpired($name))
+            return 0;
+        
+        return ($this->get($name.'_created_at') + $this->get($name.'_timeout')) - time();
+    }
 
 	/**
 	 * Exclui uma sessão
